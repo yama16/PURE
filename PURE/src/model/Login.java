09c29e34773717dfa65428167ab80499d5c57deb
fuluser <kbc17a19@stu.kawahara.ac.jp>
2018-05-28 20:31:14 +0900

    ログインする時に使うクラス

--- PURE/src/model/Login.java
+++ PURE/src/model/Login.java
@@ -1,5 +1,30 @@
+
 package model;
 
+/**
+ * ユーザーが入力したログイン情報を保持するクラス。
+ *
+ * @author furukawa
+ */
 public class Login {
+
+    /** アカウントID */
+    private String id;
+    /** パスワード */
+    private String password;
+
+    /**
+     * アカウントIDとパスワードを引数に持つコンストラクタ。
+     * @param id アカウントID
+     * @param password パスワード
+     */
+    public Login(String id, String password){
+        this.id = id;
+        this.password = password;
+    }
+
+    //getter
+    public String getId(){ return id; }
+    public String getPassword(){ return password; }
 
 }
