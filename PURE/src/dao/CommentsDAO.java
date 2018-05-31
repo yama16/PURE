107b0f1baad5066fd7b17900dbda994ebcbe0921
fuluser <kbc17a19@stu.kawahara.ac.jp>
2018-05-31 22:39:21 +0900

    最新のコメントを持ってくるメソッド

--- PURE/src/dao/CommentsDAO.java
+++ PURE/src/dao/CommentsDAO.java
@@ -56,7 +56,7 @@ public class CommentsDAO {
     		}
     	} catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             if(conn != null){
                 try {
                     conn.close();
@@ -66,6 +66,49 @@ public class CommentsDAO {
             }
         }
     	return commentList.size() == 0 ? null : commentList;
+    }
+
+    /**
+     * 引数で指定した掲示板のコメントの中から、引数で指定したコメントより新しいコメントのリストを返すメソッド。
+     *
+     * @param bulletin_board_id 検索する掲示板のID
+     * @param comment_id 最新のコメントのID
+     * @return 引数のcomment_idよりも最新のコメントのリストを返す。
+     * 引数のcomment_idよりも最新のコメントが無ければnullを返す。
+     */
+    public List<Comment> findNewComment(int bulletin_board_id, int comment_id){
+    	Connection conn = null;
+    	List<Comment> newCommentList = new ArrayList<>();
+    	try {
+    		conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+
+    		String sql = "SELECT * FROM comments WHERE bulletin_board_id = ? AND comment_id > ?";
+    		PreparedStatement pStmt = conn.prepareStatement(sql);
+    		pStmt.setInt(1, bulletin_board_id);
+    		pStmt.setInt(2, comment_id);
+    		ResultSet resultSet = pStmt.executeQuery();
+    		while(resultSet.next()){
+    			Comment comment = new Comment();
+    			comment.setId(resultSet.getInt("id"));
+    			comment.setBulletin_board_id(resultSet.getInt("bulletin_board_id"));
+    			comment.setAccount_id(resultSet.getString("account_id"));
+    			comment.setComment(resultSet.getString("comment"));
+    			comment.setCreated_at(resultSet.getTimestamp("created_at"));
+    			comment.setPure_quantity(resultSet.getInt("pure_quantity"));
+    			newCommentList.add(comment);
+    		}
+    	} catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+    	return newCommentList.size() == 0 ? null : newCommentList;
     }
 
 }
