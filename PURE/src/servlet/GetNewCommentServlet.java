bd04f950c55f21257d751550e55646fa65b7edd1
fuluser <kbc17a19@stu.kawahara.ac.jp>
2018-06-06 12:46:39 +0900

    create table（仮）修正

--- PURE/src/servlet/GetNewCommentServlet.java
+++ PURE/src/servlet/GetNewCommentServlet.java
@@ -1,12 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Comment;
+import model.GetNewCommentLogic;
 
 /**
  * Servlet implementation class GetNewCommentServlet
@@ -19,6 +23,12 @@ public class GetNewCommentServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int bulletin_board_id = Integer.parseInt(request.getParameter("bulletin_board_id"));
+		int comment_id = Integer.parseInt(request.getParameter("comment_id"));
+
+		GetNewCommentLogic logic = new GetNewCommentLogic();
+		List<Comment> newCommentList = logic.execute(bulletin_board_id, comment_id);
+
 
 	}
 
