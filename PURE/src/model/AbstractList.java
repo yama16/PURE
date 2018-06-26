38a3737b238c10e43890767ee6209c8d172566ea
fuluser <kbc17a19@stu.kawahara.ac.jp>
2018-06-26 13:24:20 +0900

    AbstractListとTagList

---
+++ PURE/src/model/AbstractList.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractList<T> {
+
+	private List<T> list;
+
+	public AbstractList(){
+		list = new ArrayList<>();
+	}
+	public AbstractList(List<T> list){
+		this.list = list;
+	}
+
+	public T get(int idx){
+		if(idx >= list.size()){
+			return null;
+		}
+		return list.get(idx);
+	}
+
+	public int size(){
+		return list.size();
+	}
+
+	public boolean add(T t){
+		if(t == null){
+			return false;
+		}
+		return list.add(t);
+	}
+
+}
