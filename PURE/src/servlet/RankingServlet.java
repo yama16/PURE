cb3462a14bc9cd43377f9b3a8aad25e981dc7ab4
keipo <kbc17a22@stu.kawahara.ac.jp>
2018-06-30 13:02:07 +0900

    RankingServlet.javaを追加

---
+++ PURE/src/servlet/RankingServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RankingServlet
+ */
+@WebServlet("/RankingServlet")
+public class RankingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/PURE//WEB-INF/jsp/ranking.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
